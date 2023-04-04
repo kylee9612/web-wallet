ce87188636f9246bdffbb3a2e6589315e56c221f
kylee9612 <iiii410@naver.com>
2023-04-04 23:29:40 +0900

    swal 추가

--- src/main/my-app/src/components/page/coin/Deposit.js
+++ src/main/my-app/src/components/page/coin/Deposit.js
@@ -1,53 +1,75 @@
-import React, {useState} from "react"
+import React, {useRef, useState} from "react"
 import axios from "axios";
+import "./deposit.css"
+import Swal from "sweetalert2";
 
 function Deposit({coin}) {
     const [coinData, setCoinData] = useState({});
+    const [qrCode, setQrCode] = useState("");
+
+    const qrRef = useRef();
+    const genRef = useRef();
 
     const generate = () => {
         axios.get("/api/v2/" + coin.toLowerCase() + "/generate")
             .then((response) => {
                 setCoinData(response.data)
+                setQrCode("data:image/png;base64," + response.data.qr_code)
+                qrRef.current.style.display = ""
+                genRef.current.style.display = "none"
+                Swal.fire({
+                    "html":
+                        "<div class='alert-div'>" +
+                        "<span>private key</span>" +
+                        "<input type='text' id='from_private' class='alert-input' value='" + response.data.private_key + "'/>" +
+                        "<br><br>" +
+                        "<span>public key</span>" +
+                        "<input type='text' id='from_public' class='alert-input' value='" + response.data.public_key + "'/>" +
+                        "</div>",
+                    "showConfirmButton" : "OK"
+                })
             })
             .catch((error) => {
                 console.log(error)
             })
-            .finally(() => {
-                console.log(coinData)
-            })
+    }
+
+    const imgError = (event) => {
+        event.target.style.display = 'none'
     }
 
     return (
         <>
             <div id="left-body" className="body_content">
-                <h2>{coin}</h2>
-                <img id={"qr_code"} src={"data:image/png;base64,"+coinData.qr_code} width={"400px"} height={"400px"} alt={'qr_code'}/>
-                {/*<img id={"qr_code"} src={"data:image/png;base64,"+prop.data.qr_code} width={"400px"} height={"400px"} alt={'qr_code'}/>*/}
-                <label htmlFor="from_public">Public Key
-                    <input type="text" id="from_public" className="in_body" placeholder="publicKey"/>
-                </label>
-                <label htmlFor="from_private">Private Key
-                    <input type="text" id="from_private" className="in_body" placeholder="privateKey"/>
-                </label>
+                <div className={"content-top"}>
+                    <h2>{coin}</h2>
+                </div>
+                <div className={"qr_code"}>
+                    <img ref={qrRef} src={qrCode} width={"300px"} height={"300px"} alt={'qr_code'}
+                         onError={imgError}/>
+                    <button ref={genRef} id="generate" onClick={generate} className="in_body">Generate Wallet</button>
+                </div>
+                {/*<label htmlFor="from_public">Public Key*/}
+                {/*    <input type="text" id="from_public" className="in_body" placeholder="publicKey"/>*/}
+                {/*</label>*/}
+                {/*<label htmlFor="from_private">Private Key*/}
+                {/*    <input type="text" id="from_private" className="in_body" placeholder="privateKey"/>*/}
+                {/*</label>*/}
 
                 <label htmlFor="from_address">Classic Address
-                    <input type="text" id="from_address" className="in_body" placeholder="classicAddress"/>
+                    <input type="text" id="from_address" className="in_body" placeholder="Address"
+                           value={coinData.address} readOnly/>
                 </label>
 
                 <label htmlFor="from_tag">Destination Tag
-                    <input type="text" id="from_tag" className="in_body" placeholder="Destination Tag"/>
+                    <input type="text" id="from_tag" className="in_body" placeholder="Destination Tag"
+                           value={coinData.destination} readOnly/>
                 </label>
 
                 <label htmlFor="from_balance">balance
-                    <input type="text" id="from_balance" className="in_body" readOnly="readonly"/>
+                    <input type="text" id="from_balance" className="in_body" readOnly/>
                 </label>
-                <label htmlFor="from_valid">
-                    <button id="from_valid_button" className="in_body">Check Valid</button>
-                    <input type="text" id="from_valid" className="in_small" readOnly="readonly"/>
-                </label>
-                <label>
-                    <button id="generate" onClick={generate} className="in_body">Generate Wallet</button>
-                </label>
+                <button id="generate" onClick={generate} className="in_body">Send Asset</button>
             </div>
         </>
     )
